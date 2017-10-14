1855ed0f9e7b45052eaa7065d89f68dd5aa6925b
Michael d <mdukes00@live.com>
2017-10-14 13:49:39 +0200

    taking a break

--- DCMotorTest.ino
+++ DCMotorTest.ino
@@ -1,11 +1,14 @@
+
+
+#include <Stream.h>
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
 #include "utility/Adafruit_MS_PWMServoDriver.h"
 
 // Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61);
 
 // Select which 'port' M1, M2, M3 or M4. In this case, M1
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
@@ -18,79 +21,100 @@ void setup() {
 
   AFMS.begin();  // create with the default frequency 1.6KHz
   //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
-  
+
   // Set the speed to start, from 0 (off) to 255 (max speed)
-  
+
 
 }
 
-void loop() { 
-  
-Forward();
-delay(500);
-
-TurnLeft();
-delay(500);
-
-DriveLeft();
-delay(500);
-
-TurnRight();
-delay(500);
-
-DriveRight();
-delay(500);
-
-Backward();
-delay(500);
-
-Stop();
-delay(500);
-
+void loop() {
+  char keystroke;
+  if (Serial.available() > 0) {
+    keystroke = Serial.read();
+    switch (keystroke) {
+      case '1':
+        Serial.println("Forward");
+        break;
+      case '2':
+        Serial.println("Backward");
+        break;
+      case '3':
+        Serial.println("TurnLeft");
+        break;
+      case '4':
+        Serial.println("DriveLeft");
+        break;
+      case '5':
+        Serial.println("TurnRight");
+        break;
+      case '6':
+        Serial.println("DriveRight");
+        break;
+      case '7':
+        Serial.println("Stop");
+        break;
+      case '8':
+        Serial.println("Destory all other robots....");
+        break;
+      default:
+        // if nothing else matches, do the default
+        // default is optional
+        break;
+    }
   }
 
-void SetSpeed(int right, int left){
+}
+
+
+
+
+
+
+
+
+
+void SetSpeed(int right, int left) {
   leftMotor->setSpeed(left);
   rightMotor->setSpeed(right);
 }
 
-void Forward(){
-  SetSpeed(255,255);
+void Forward() {
+  SetSpeed(255, 255);
   rightMotor->run(FORWARD);
   leftMotor->run(FORWARD);
 }
 
-void Backward(){
-  SetSpeed(255,255);
+void Backward() {
+  SetSpeed(255, 255);
   rightMotor->run(BACKWARD);
   leftMotor->run(BACKWARD);
 }
-void TurnLeft(){
-  SetSpeed(255,255); 
+void TurnLeft() {
+  SetSpeed(255, 255);
   rightMotor->run(FORWARD);
   leftMotor->run(BACKWARD);
 }
 
-void TurnRight(){
-  SetSpeed(255,255);
+void TurnRight() {
+  SetSpeed(255, 255);
   rightMotor->run(BACKWARD);
   leftMotor->run(FORWARD);
 }
 
-void DriveLeft(){
-  SetSpeed(255,50);
+void DriveLeft() {
+  SetSpeed(255, 50);
   rightMotor->run(FORWARD);
   leftMotor->run(FORWARD);
 }
 
-void DriveRight(){
-  SetSpeed(50,255);
+void DriveRight() {
+  SetSpeed(50, 255);
   rightMotor->run(FORWARD);
   leftMotor->run(FORWARD);
 }
 
-void Stop(){
-  SetSpeed(0,0);
+void Stop() {
+  SetSpeed(0, 0);
   rightMotor->run(RELEASE);
   leftMotor->run(RELEASE);
 }
